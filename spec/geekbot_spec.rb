8d1a8c3f4e435cbd04b121c31f77d5567f6e97ff
Pegasus204 <tenma.fukaya@misoca.jp>
2017-12-06 15:54:07 +0900

    必ずコケるspecを削除した

--- spec/geekbot_spec.rb
+++ spec/geekbot_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Geekbot do
   it 'has a version number' do
     expect(Geekbot::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
