7f222d3cc271fe3a25eaab97a9dfabea9326f413
Pegasus204 <tenma.fukaya@misoca.jp>
2017-12-07 18:04:37 +0900

    引数を間違ったときなどにBad URIが出て原因がわかりにくかったので引数名の宣言を必須にした

--- lib/geekbot/api_methods.rb
+++ lib/geekbot/api_methods.rb
@@ -1,34 +1,34 @@
 module Geekbot
   module ApiMethods
-    def index_teams(params = nil, headers = nil)
+    def index_teams(params: nil, headers: nil)
       get('/v1/teams', params, headers)
     end
 
-    def index_reports(params = nil, headers = nil)
+    def index_reports(params: nil, headers: nil)
       get('/v1/reports', params, headers)
     end
 
-    def index_standups(params = nil, headers = nil)
+    def index_standups(params: nil, headers: nil)
       get('/v1/standups', params, headers)
     end
 
-    def show_standup(id = nil, params = nil, headers = nil)
+    def show_standup(id:, params: nil, headers: nil)
       get("/v1/standups/#{id}", params, headers)
     end
 
-    def create_standup(params = nil, headers = nil)
+    def create_standup(params: nil, headers: nil)
       post('/v1/standups', params, headers)
     end
 
-    def update_standup(id, params = nil, headers = nil)
+    def update_standup(id:, params: nil, headers: nil)
       patch("/v1/standups/#{id}", params, headers)
     end
 
-    def replace_standup(id, params = nil, headers = nil)
+    def replace_standup(id:, params: nil, headers: nil)
       put("/v1/standups/#{id}", params, headers)
     end
 
-    def destroy_standup(id, params = nil, headers = nil)
+    def destroy_standup(id:, params: nil, headers: nil)
       delete("/v1/standups/#{id}", params, headers)
     end
   end
